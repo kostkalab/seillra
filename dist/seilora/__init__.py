b91ad6fbd10e4fe6da36a35bfef1eeb0789156ff
Dennis Kostka <kostka@pitt.edu>
2025-09-05 13:37:06 -0400

    Automated publish for version 1.0.8

--- dist/seilora/__init__.py
+++ dist/seilora/__init__.py
@@ -1,7 +1,8 @@
 from .src.get_models import *
 from .src.sei_head_lora import *
+from .src.model_loading import download_file_atomic
 # Optionally, define __all__ for explicit exports
 __all__ = [
     'SeiHeadLora',
-    'get_sei_trunk_q', 'get_sei_head_lora'
+    'get_sei_trunk_q', 'get_sei_head_lora', 'download_file_atomic'
 ]
