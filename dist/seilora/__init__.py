a87068ac36c0ae5506ef56a7f7cabd479581d846
Dennis Kostka <kostka@pitt.edu>
2025-09-05 11:19:00 -0400

    Automated publish for version 1.0.1

---
+++ dist/seilora/__init__.py
@@ -0,0 +1,7 @@
+from .src.get_models import *
+from .src.sei_head_lora import *
+# Optionally, define __all__ for explicit exports
+__all__ = [
+    'SeiHeadLora',
+    'get_sei_trunk_q', 'get_sei_head_lora'
+]
