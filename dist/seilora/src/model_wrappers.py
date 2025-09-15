c13385a9ac037adc251b937d86bee6d93d85370d
Elizabeth Gilfeather <elizabeth.gilfeather@gmail.com>
2025-09-15 16:08:40 -0400

    Automated publish for version 1.0.16

--- dist/seilora/src/model_wrappers.py
+++ dist/seilora/src/model_wrappers.py
@@ -15,7 +15,7 @@ import os, sys
 
 # from seimodel.src import get_seimodels as sm
 class SeiLoraWrapper(nn.Module):
-    def __init__(self, k: int, projection = True, mode = "sequence"):
+    def __init__(self, k: int, projection = True, mode = "sequence", device = "cpu"):
         super().__init__()
 
         self.mode = mode
@@ -25,6 +25,7 @@ class SeiLoraWrapper(nn.Module):
         if self.projection:
             self.proj = sm.get_sei_projection().load_weights()
             self.proj.set_mode(mode)
+        self.device = device
 
     def set_mode(self, mode):
         if mode != "sequence" and mode != "variant":
@@ -38,16 +39,20 @@ class SeiLoraWrapper(nn.Module):
         Forward pass: computes output for both original and reversed input
         and averages the results. This is fed into the projector.
         """
+        self.qtrunk = self.qtrunk.to("cpu")
         if self.projection:
             if self.projection.mode == "variant":
                 x_r, x_a = x
+                x_r, x_a = x_r.to("cpu"), x_a.to("cpu")
                 with torch.no_grad():
                     for_x_r = self.qtrunk(x_r)
+                for_x_r = for_x_r.to(self.device)
                 for_x_r = self.head(for_x_r)
 
                 rev_x_r = torch.flip(x_r, dims=[1, 2])
                 with torch.no_grad():
                     rev_x_r = self.qtrunk(rev_x_r)
+                rev_x_r = rev_x_r.to(self.device)
                 rev_x_r = self.head(rev_x_r)
 
                 out_r = (for_x_r + rev_x_r) / 2
@@ -55,24 +60,29 @@ class SeiLoraWrapper(nn.Module):
 
                 with torch.no_grad():
                     for_x_a = self.qtrunk(x_a)
+                for_x_a = for_x_a.to(self.device)
                 for_x_a = self.head(for_x_a)
 
                 rev_x_a = torch.flip(x_a, dims=[1, 2])
                 with torch.no_grad():
                     rev_x_a = self.qtrunk(rev_x_a)
+                rev_x_a = rev_x_a.to(self.device)
                 rev_x_a = self.head(rev_x_a)
 
                 out_a = (for_x_a + rev_x_a) / 2
                 out = (out_r, out_a)
                 out = self.proj(out)
             else: ## default to sequence
+                x = x.to("cpu")
                 with torch.no_grad():
                     for_x = self.qtrunk(x)
+                for_x = for_x.to(self.device)
                 for_x = self.head(for_x)
 
                 rev_x = torch.flip(x, dims=[1, 2])
                 with torch.no_grad():
                     rev_x = self.qtrunk(rev_x)
+                rev_x = rev_x.to(self.device)
                 rev_x = self.head(rev_x)
 
                 out = (for_x + rev_x) / 2
@@ -80,13 +90,16 @@ class SeiLoraWrapper(nn.Module):
         else:
             if self.mode == "variant":
                 x_r, x_a = x
+                x_r, x_a = x_r.to("cpu"), x_a.to("cpu")
                 with torch.no_grad():
                     for_x_r = self.qtrunk(x_r)
+                for_x_r = for_x_r.to(self.device)
                 for_x_r = self.head(for_x_r)
 
                 rev_x_r = torch.flip(x_r, dims=[1, 2])
                 with torch.no_grad():
                     rev_x_r = self.qtrunk(rev_x_r)
+                rev_x_r = rev_x_r.to(self.device)
                 rev_x_r = self.head(rev_x_r)
 
                 out_r = (for_x_r + rev_x_r) / 2
@@ -94,23 +107,28 @@ class SeiLoraWrapper(nn.Module):
 
                 with torch.no_grad():
                     for_x_a = self.qtrunk(x_a)
+                for_x_a = for_x_a.to(self.device)
                 for_x_a = self.head(for_x_a)
 
                 rev_x_a = torch.flip(x_a, dims=[1, 2])
                 with torch.no_grad():
                     rev_x_a = self.qtrunk(rev_x_a)
+                rev_x_a = rev_x_a.to(self.device)
                 rev_x_a = self.head(rev_x_a)
 
                 out_a = (for_x_a + rev_x_a) / 2
                 out = (out_r, out_a)
             else:
+                x = x.to("cpu")
                 with torch.no_grad():
                     for_x = self.qtrunk(x)
+                for_x = for_x.to(self.device)
                 for_x = self.head(for_x)
 
                 rev_x = torch.flip(x, dims=[1, 2])
                 with torch.no_grad():
                     rev_x = self.qtrunk(rev_x)
+                rev_x = rev_x.to(self.device)
                 rev_x = self.head(rev_x)
 
                 out = (for_x + rev_x) / 2
