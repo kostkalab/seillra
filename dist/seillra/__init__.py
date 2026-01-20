fd92953f235c59a5b77333d97d42ab8609bc8386
Elizabeth Gilfeather <elizabeth.gilfeather@gmail.com>
2026-01-20 15:54:50 -0500

    Automated publish for version 1.4.2

--- dist/seillra/__init__.py
+++ dist/seillra/__init__.py
@@ -1,10 +1,9 @@
 from seillra.src.get_models import *
-from seillra.src.sei_head_llra import *
 from seillra.src.model_loading import download_file_atomic
 from seillra.src.model_wrappers import *
 from seillra.src.sei_parts import *
 # Optionally, define __all__ for explicit exports
 __all__ = [
     'SeiHeadLLRA',
-    'get_sei_trunk_q', 'get_sei_head_llra', 'download_file_atomic', 'Sei_LLRA'
+    'get_sei_trunk', 'get_sei_head_llra', 'download_file_atomic', 'Sei_LLRA'
 ]
