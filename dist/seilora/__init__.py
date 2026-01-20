c216e2d2d2edbab9fe9f62c0499b26d7f974e183
Dennis Kostka <kostka@pitt.edu>
2026-01-20 16:27:11 -0500

    Deleted the seilora dirs from before the rename

--- dist/seilora/__init__.py
+++
@@ -1,9 +0,0 @@
-from .src.get_models import *
-from .src.sei_head_llra import *
-from .src.model_loading import download_file_atomic
-from .src.model_wrappers import *
-# Optionally, define __all__ for explicit exports
-__all__ = [
-    'SeiHeadLLRA',
-    'get_sei_trunk_q', 'get_sei_head_llra', 'download_file_atomic', 'Sei_LLRA'
-]
